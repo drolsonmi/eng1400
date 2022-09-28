b1dabf0a0cfb2476f2c8d3211db345251137461b
drolsonmi <michael.olson2@snow.edu>
2022-09-28 14:02:31 -0600

    Updated lecture slides Ch 3,4

---
+++ Dice_Roll.cpp
@@ -0,0 +1,19 @@
+/*
+
+This program will create two random numbers between
+1 and 6, simulating 2 dice being thrown.
+
+Algorithm:
+1. 
+2. 
+3. 
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
